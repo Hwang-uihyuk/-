c39500a2082c9d15ae3a1bcabbad6e3cee064d64
Hwang-uihyuk <weihyuk39@gmail.com>
2022-02-05 13:11:39 +0900

    11557

---
+++ 11557.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    max_res = 0
+    for _ in range(n):
+        a,b = input().split()
+        b = int(b)
+        if max_res < b:
+            res_a = a 
+            max_res = int(b)
+
+    print(res_a)
